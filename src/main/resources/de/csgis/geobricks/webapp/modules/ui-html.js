5d4ad54b7bec6ef272dff89cec7786208fa9fb3f
Víctor González <victor.gonzalez@geomati.co>
2014-08-12 08:23:19 +0200

    Rename ui-html event to ui-html:create. Fixes #1173

--- src/main/resources/de/csgis/geobricks/webapp/modules/ui-html.js
+++ src/main/resources/de/csgis/geobricks/webapp/modules/ui-html.js
@@ -1,5 +1,5 @@
 define([ "jquery", "message-bus", "ui-commons" ], function($, bus, commons) {
-	bus.listen("ui-html", function(e, msg) {
+	bus.listen("ui-html:create", function(e, msg) {
 		var div = commons.getOrCreateDiv(msg.div, msg.parentDiv);
 		div.html(msg.html);
 		div.addClass(msg.css);
