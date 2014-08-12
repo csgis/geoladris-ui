2e4a2bc16bb23a55b846aec14e2c369769d211b7
Víctor González <victor.gonzalez@geomati.co>
2014-08-12 08:26:46 +0200

    Remove ui-container module. Use ui-html instead

--- src/main/resources/de/csgis/geobricks/webapp/modules/ui-container.js
+++
@@ -1,6 +0,0 @@
-define([ "jquery", "message-bus" ], function($, bus) {
-	bus.listen("ui-container:create", function(e, msg) {
-		var container = $("<div/>").attr("id", msg.div).addClass(msg.css);
-		$("#" + msg.parentDiv).append(container);
-	});
-});
