761c37bac882748e23eeb71d05ecb7b48f4bb51b
Víctor González <victor.gonzalez@geomati.co>
2014-12-18 09:37:15 +0100

    Add expand/collapse/toggle events to sliding div. Refs #996

--- src/main/resources/webapp/modules/ui-sliding-div.js
+++ src/main/resources/webapp/modules/ui-sliding-div.js
@@ -1,6 +1,43 @@
-define([ "jquery", "message-bus" ], function($, bus) {
+define([ "jquery", "message-bus", "module" ], function($, bus, module) {
 	var CLOSED_CLASS = "handle-closed";
 	var OPENED_CLASS = "handle-opened";
+
+	var HANDLE_CLASS = "ui-sliding-div-handle";
+
+	var config = module.config();
+
+	if (!config.duration) {
+		config.duration = 0;
+	}
+
+	function expand(id) {
+		var div = $("#" + id);
+		var handle = div.siblings("." + HANDLE_CLASS);
+
+		div.slideDown(config.duration);
+		handle.addClass(OPENED_CLASS);
+		handle.removeClass(CLOSED_CLASS);
+	}
+
+	function collapse(id) {
+		var div = $("#" + id);
+		var handle = div.siblings("." + HANDLE_CLASS);
+
+		div.slideUp(config.duration);
+		handle.addClass(CLOSED_CLASS);
+		handle.removeClass(OPENED_CLASS);
+	}
+
+	function toggle(id) {
+		var div = $("#" + id);
+		var handle = div.siblings("." + HANDLE_CLASS);
+
+		if (handle.hasClass(CLOSED_CLASS)) {
+			expand(id);
+		} else {
+			collapse(id);
+		}
+	}
 
 	bus.listen("ui-sliding-div:create", function(e, msg) {
 		// Container
@@ -18,20 +55,22 @@ define([ "jquery", "message-bus" ], function($, bus) {
 
 		// Handle div
 		var handle = $("<div/>");
+		handle.addClass(HANDLE_CLASS);
 		handle.addClass(CLOSED_CLASS);
-		handle.addClass("ui-sliding-div-handle");
 		container.append(handle);
 
 		handle.click(function() {
-			if (handle.hasClass(CLOSED_CLASS)) {
-				div.slideDown();
-				handle.addClass(OPENED_CLASS);
-				handle.removeClass(CLOSED_CLASS);
-			} else {
-				div.slideUp();
-				handle.addClass(CLOSED_CLASS);
-				handle.removeClass(OPENED_CLASS);
-			}
+			toggle(msg.div);
 		});
+	});
+
+	bus.listen("ui-sliding-div:collapse", function(e, id) {
+		collapse(id);
+	});
+	bus.listen("ui-sliding-div:expand", function(e, id) {
+		expand(id);
+	});
+	bus.listen("ui-sliding-div:toggle", function(e, id) {
+		toggle(id);
 	});
 });
--- src/test/resources/unit-test/test-sliding-div.js
+++ src/test/resources/unit-test/test-sliding-div.js
@@ -7,7 +7,10 @@ describe("ui-sliding-div", function() {
 		_bus.unbind();
 		spyOn(_bus, "send").and.callThrough();
 
-		_initModule("ui-sliding-div", [ $, _bus ]);
+		var module = jasmine.createSpyObj("config", [ "config" ]);
+		module.config.and.returnValue({});
+
+		_initModule("ui-sliding-div", [ $, _bus, module ]);
 	});
 
 	it("creates a handle and a content div within a container on create", function() {
@@ -37,5 +40,49 @@ describe("ui-sliding-div", function() {
 		expect(content.is(':visible')).toBe(false);
 		handle.click();
 		expect(content.is(':visible')).toBe(true);
+	});
+
+	it("shows on expand event", function() {
+		_bus.send("ui-sliding-div:create", {
+			div : "mysliding",
+			parentDiv : parentId
+		});
+
+		var div = $("#mysliding");
+
+		expect(div.is(':visible')).toBe(false);
+		_bus.send("ui-sliding-div:expand", "mysliding");
+		expect(div.is(':visible')).toBe(true);
+	});
+
+	it("hides on collapse event", function() {
+		_bus.send("ui-sliding-div:create", {
+			div : "mysliding",
+			parentDiv : parentId
+		});
+
+		var div = $("#mysliding");
+		var handle = div.siblings(".ui-sliding-div-handle");
+
+		handle.click();
+
+		expect(div.is(':visible')).toBe(true);
+		_bus.send("ui-sliding-div:collapse", "mysliding");
+		expect(div.is(':visible')).toBe(false);
+	});
+
+	it("toggles on toggle event", function() {
+		_bus.send("ui-sliding-div:create", {
+			div : "mysliding",
+			parentDiv : parentId
+		});
+
+		var div = $("#mysliding");
+
+		expect(div.is(':visible')).toBe(false);
+		_bus.send("ui-sliding-div:toggle", "mysliding");
+		expect(div.is(':visible')).toBe(true);
+		_bus.send("ui-sliding-div:toggle", "mysliding");
+		expect(div.is(':visible')).toBe(false);
 	});
 });
